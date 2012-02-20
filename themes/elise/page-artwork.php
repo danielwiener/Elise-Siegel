6baf97453b5e5be8589d334d25fc0fa433e74700
danielwiener <daniel@danielwiener.com>
2012-02-20 11:57:02 -0500

    working on artwork page - adding template name and other info

--- themes/elise/page-artwork.php
+++ themes/elise/page-artwork.php
@@ -1,11 +1,8 @@
 <?php
 /**
- * The template for displaying all pages.
+ * The template for displaying single artworks with related thumbnails on the right.
  *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site will use a
- * different template.
+ * Template Name: Artwork Page
  *
  * @package WordPress
  * @subpackage Elise Siegel
