7ae964a2c9b99228e9055c8070ed7425ab1e58d5
danielwiener <daniel@danielwiener.com>
2012-02-28 12:22:47 -0500

    deleted footer stuff since there is none

--- themes/elise/footer.php
+++ themes/elise/footer.php
@@ -12,24 +12,6 @@
 ?>
 	</div><!-- #main -->
 
-	<div id="footer" role="contentinfo">
-		<div id="colophon">
-
-<?php
-	/* A sidebar in the footer? Yep. You can can customize
-	 * your footer with four columns of widgets.
-	 */
-	get_sidebar( 'footer' );
-?>
-
-			<div id="site-info">
-			</div><!-- #site-info -->
-			<div id="site-generator"> 		  
-			</div><!-- #site-generator -->
-
-		</div><!-- #colophon -->
-	</div><!-- #footer -->
-
 </div><!-- #wrapper -->
 
 <?php
