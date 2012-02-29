62f843322eb4fa15c6d1b1f3a4e934c2a889ba07
danielwiener <daniel@danielwiener.com>
2012-02-29 10:01:31 -0500

    setting up front page for a single image that takes up the width of the page

--- themes/elise/front-page.php
+++ themes/elise/front-page.php
@@ -15,63 +15,15 @@ get_header(); ?>
 
 		<div id="container" class="single-attachment">
 			<div id="content" role="main">
-				<div class="images"> 
-                 
-		          	 <?php
-					     $slidetabs = '';
-							$args = array(
-								'order'          => 'ASC',
-								'orderby' 		 => 'menu_order',
-								'post_type'      => 'attachment',
-								'post_parent'    => $post->ID,
-								'post_mime_type' => 'image',
-								'post_status'    => null,
-								'numberposts'    => -1,
-							);
-
-							$attachments = get_posts($args);
-							if ($attachments) :?> 
-							<?php foreach ($attachments as $attachment) : ?>  
-                                <div>
-						   <a href="<?php the_permalink(); ?>"><?php echo wp_get_attachment_image($attachment->ID, 'full', false, false); ?></a>                            
-
-                             </div> <!-- plain div -->  
-						<?php $slidetabs .= '<a href="#"></a>'; ?>	
-						<?php endforeach; ?>
-								
-							<?php endif; ?>
-			      
-				   <div class="slidetabs">
-						<?php echo $slidetabs ?>
-						</div>
-
-					<!-- <div class="buttons">
-						<button onClick='jQuery(".slidetabs").data("slideshow").play();'>Play</button>
-						<button onClick='jQuery(".slidetabs").data("slideshow").stop();'>Stop</button>
-					</div> -->
-
-
-					<script language="JavaScript">
-					// What is $(document).ready ? See: http://flowplayer.org/tools/documentation/basics.html#document_ready
-					jQuery.noConflict();
-					jQuery(function()  {
-
-					jQuery(".slidetabs").tabs(".images > div", {
-
-						// enable "cross-fading" effect
-						effect: 'fade',
-						fadeOutSpeed: "slow",
-
-						// start from the beginning after the last tab
-						rotate: true
-
-					// use the slideshow plugin. It accepts its own configuration
-					}).slideshow({autoplay:true});
-					});
-					</script>
-
-				   
-             </div> <!-- #images -->
+				<?php if( have_posts() ) {
+					while ( have_posts() ) {
+						the_post();
+						the_content();
+					}
+				} else {
+					/* No posts found */
+				} ?>
+			   
 			</div><!-- #content -->
 		</div><!-- #container -->
 
